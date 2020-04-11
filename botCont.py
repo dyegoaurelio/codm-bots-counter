d90bebf876b4f436e529159c5a1d4206d5d52c98
dyego <dyegoaurelio@gmail.com>
2020-04-11 16:47:44 -0300

    melhorias de organizacao

--- botCont.py
+++ botCont.py
@@ -11,11 +11,21 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 #criando os comandos
 def Start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-    
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I can count how many bots are on your Call of Duty Mobile match\nJust send me a screenshot of the scoreboard??\nFor additional information simply type /about")
 
-def Teste(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="outro comando /teste")
+def About(update,context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Maybe you are wondering how do I guess who looks like a bot and who doesnt")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Thats actually not that hard\nUsually bots have the same names, so I gathered information about their names playing many times at Pratice Against AI, so I guess im a snitch Bot")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="If you found someone that I thought was a bot and you disagree let me know at /feedback  ??")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="If you enjoyed my work you can support me at /donate , I really would appreciate that")
+
+
+def Donate(update,context):
+    pass
+
+def Feedback(update,context):
+    pass
+
 
 from ocr import bot_counter
 from os import remove as delete
@@ -25,15 +35,22 @@ def readRawImage(update, context):
     path = file.file_id+'.jpeg'
     file.download(path)
     result = bot_counter(path)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=str(result))
+    if (len(result)>0):
+        texto = 'There are at least ' +str (len(result))+ ' bots in this match\n'
+        for i in result: #aprendendo o texto
+            texto = texto + i + ' ,' 
+        texto = texto[:-2] #removendo a ultima virgula
+    else: #texto para se n tiver bot
+        texto = "I couldn't find any bot in this match ????"
+    context.bot.send_message(chat_id=update.effective_chat.id, text= texto , reply_to_message_id =update.message.message_id)
     delete(path)
     
 #fazer eles serem chamados
 from telegram.ext import CommandHandler 
 
 dispatcher.add_handler(CommandHandler('start', Start))
+dispatcher.add_handler(CommandHandler('about', About))
 
-dispatcher.add_handler(CommandHandler('teste', Teste))
 
 
 from telegram.ext import MessageHandler, Filters
