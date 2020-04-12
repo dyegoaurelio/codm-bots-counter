e54c368244c0ff123096ffcb82949c235f618e70
dyego <dyegoaurelio@gmail.com>
2020-04-11 23:45:59 -0300

    ajeitado

--- botCont.py
+++ botCont.py
@@ -14,11 +14,14 @@ def Start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I can count how many bots are on your Call of Duty Mobile match\nJust send me a screenshot of the scoreboard📸\nFor additional information simply type /about")
 
 def About(update,context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="If you are confused about the usage of this application type /tutorial")
     context.bot.send_message(chat_id=update.effective_chat.id, text="Maybe you are wondering how do I guess who looks like a bot and who doesnt")
     context.bot.send_message(chat_id=update.effective_chat.id, text="Thats actually not that hard\nUsually bots have the same names, so I gathered information about their names playing many times at Pratice Against AI, so I guess im a snitch Bot")
     context.bot.send_message(chat_id=update.effective_chat.id, text="If you found someone that I thought was a bot and you disagree let me know at /feedback  👍")
     context.bot.send_message(chat_id=update.effective_chat.id, text="If you enjoyed my work you can support me at /donate , I really would appreciate that")
 
+def Tutorial(update,context):
+    pass
 
 def Donate(update,context):
     pass
@@ -31,26 +34,40 @@ from ocr import bot_counter
 from os import remove as delete
 
 def readRawImage(update, context):
-    file =  context.bot.getFile(update.message.photo[-1].file_id)
+    file =  context.bot.getFile(update.message.photo[-1].file_id)   #recive the image url on telegram server
     path = file.file_id+'.jpeg'
-    file.download(path)
-    result = bot_counter(path)
-    if (len(result)>0):
-        texto = 'There are at least ' +str (len(result))+ ' bots in this match\n'
-        for i in result: #aprendendo o texto
-            texto = texto + i + ' ,' 
-        texto = texto[:-2] #removendo a ultima virgula
-    else: #texto para se n tiver bot
-        texto = "I couldn't find any bot in this match 🤔🤔"
+    file.download(path) #download image with a unique name
+    
+    
+    try:
+        result = bot_counter(path) #try to recive 2 lists with the botnames by analyzing the downloaded image
+        
+    except RuntimeError:
+        texto = "That definitely insn't a CODM match screenshot😫\nIf you are confused about the usage of this application type /tutorial"
+        
+        delete(path) #delete the image
+    else:
+        delete(path) #delete the image if didnt raise error 
+        
+        if (len(result[0])+len(result[1])>0): #aprendendo o texto, pegando as duas listas
+                                              #cada lista representa um time
+            texto = 'There are at least ' +str (len(result[0])+len(result[1]))+ ' bots in this match\n'
+            for lista in result:
+                for i in lista: 
+                    texto = texto + i + ' ,' 
+                texto = texto[:-2] + "\n" #removendo a ultima virgula e quebrando a linha
+        else: #texto para se n tiver bot
+            texto = "I couldn't find any bot in this match 🤔🤔"
     context.bot.send_message(chat_id=update.effective_chat.id, text= texto , reply_to_message_id =update.message.message_id)
-    delete(path)
     
 #fazer eles serem chamados
 from telegram.ext import CommandHandler 
 
 dispatcher.add_handler(CommandHandler('start', Start))
 dispatcher.add_handler(CommandHandler('about', About))
-
+dispatcher.add_handler(CommandHandler('tutorial', Tutorial))
+dispatcher.add_handler(CommandHandler('feedback', Feedback))
+dispatcher.add_handler(CommandHandler('donate', Donate))
 
 
 from telegram.ext import MessageHandler, Filters
