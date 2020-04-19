dbd7763a962a0797de1a0ca4d2148d91e62ba21d
dyego <dyegoaurelio@gmail.com>
2020-04-18 22:16:44 -0300

    final

--- ocr.py
+++ ocr.py
@@ -1,33 +1,124 @@
-def bot_counter(path, width, lista_nomes = ['180Horrific', '1AParquet', '1Trippin', '2cute2quit', '45SnootyM4st3r', '4poth4c4ry', '808ClutchPoker', '808CrapsCrus', '81Sunset06', 'AbacateAereo', 'Abcoukits', 'AbutrePomelo', 'aguacatedeaire', 'AipoLamacento', 'aiRAvoCAdo', 'AIRKANGargo', 'AkanaClan', 'AlfaceFria', 'Aneaubokchoy', 'Aneaujaque', 'AniseAntes', 'Anklebiter', 'antCAbbaGE', 'AOaOaEatX0x0x', 'apiodelodo', 'Asinobokchoy', 'Asinogiaca', 'Austernlauch', 'AveRAGEleMON', 'avgJOE', 'Avocadoaria', 'Avocataerien', 'AXaAxel', 'AyeKlutch', 'backscratcher', 'BaleiaPimenta', 'Balenapiperita', 'Ballenasoleada', 'Baumgecko', 'BaxterRama', 'BillyBaller', 'BIRdARTichOKE', 'blazing', 'BobBodding', 'BObObObAngels', 'Bokchoyasno', "Bold'ours", 'Boldegombo', 'Boldemanioc', 'Bombboys', 'BOWIBEAR', 'bOWICASSava', 'BriGuy40', 'Bruddah5', 'BunsenBurner', 'BurroAcelga', 'BustedKneeCap', 'campFTW', 'Canedaikon', 'Cangrejotomate', 'CantxXxSavage', 'CaranguejoPera', 'Carsmitter', 'Carsonia', 'Cart808s', 'cassAvaRoOTS', 'CasseteTaper', 'Catalyze1', 'catCANtaloUPE', 'Cavallettarapa', 'CavaloAbobora', 'Cebollaraton', 'Cebratrueno', 'Celerisauteur', 'Celerivaseux', 'CELeryJumping', 'Cerisesalee', 'Chaosexists', 'Chayotepapagei', 'CHErrysALT', 'Chicharosfresa', 'Chienaudaikon', 'Chougras', 'ChromeBling', 'CiamBamFam', 'CinnamonSugar', 'CipherZero', 'CitrusSting', 'ClamBamFam', 'clownsey', 'CobraGengibre', 'COCOnutWEasel', 'CoioteLimao', 'COLDletTUCe', 'cOLDvilLAIN', 'ColeRunner', 'Colhormiga', 'Comadrejacoco', 'Commentsten', 'cougar7', 'Coyotelimon', 'Coyotelimone', 'Crabealapoire', 'Crabealatomate', 'Crabeheroique', 'craBHero', 'CraziiSnooty', 'CrookedTeeth', 'CrustedPie', 'cupBUMblebee', 'Cyndnay', 'd4rksOrc3rOr', 'daggerone8', 'Daikon-Frosch', 'DAIkONFROg', 'Daikonperro', 'DAlkONfROg', 'Delfindurian', 'Delfinkurbis', 'Delfinozucca', 'DeltaT', 'Digitpti', 'DIRTAngry', 'DogDaikon', 'Dogs4Lyf3', 'DoloresKaffe', 'DOLPHinSQUASH', 'Domin4ter', 'DoninhaCoco', 'Donnerzebra', 'dontshootme', 'Dragonreveur', 'dREAMdraGON', 'DreamerDonna', 'DrumerSporter', 'Eastnewyorkgee', 'edWINftw', 'ElbowDrop', 'EliteNobleman', 'Esel-Chicoree', 'F82Fei', 'FaceMelter', 'Falconespola', 'FastFanatic22', 'fATCabbAGE', 'FelonyFeline', 'Fichiveloci', 'fiGSfaST', 'FilmCritic884', 'Firehala', 'FireWalker', 'Formicacavolo', 'Formicavolo', 'Fourmisaukiwi', 'FOXsnow', 'FriedChicken', 'Fungoorso', 'Furettococco', 'Furiadetierra', 'GafanhotoNabo', 'Gattocantalupo', 'Geckoarboreo', 'Geckofruitier', 'ggBOOYAKAgg', 'GirafaAmeixa', 'Granchiopera', 'GRAPEfruitaNT', 'gRASSmanGO', 'guaVACOW', 'Guavenkuh', 'Gunschidri', 'Guywick', 'gymrat1987', 'Halconnispero', 'HamCroissant', 'Happybo', 'HAPPYcorn', 'Happyrain', 'HazMatt', 'heROLeeks', 'Heurexheros', 'Himbeerkafer', 'HimmeIsHummer', 'Homardceleste', 'Homardeclair', 'HONEYdewBADGER', 'Honigtau-Dechs', 'Hooray', 'Hormigauva', 'HORSESQUASH', 'Hummerblitz', 'HypnoTYZ', 'iLLyeaH', 'Ingwerschlange', 'insecteaugombo', 'Insectookra', 'InsetoQuiabo', 'Insettoocra', 'JacareAbobora', 'Jameston46', 'JamminJerry', 'Jennifer7', 'jimbolaya', 'Jingerschlange', 'Jirafaciruela', 'JOYSTERLEES', 'Jumper62', 'kaijustomper', 'KalteOrange', 'Khakitaube', 'Kidd0', 'KillerLi', 'Kingslayer777', 'Kirschsalz', 'kiwiANT', 'KiwiChuvoso', 'Kiwidepluie', 'Kiwilluvia', 'KneelDown', 'Kool21', 'Kumquafreddo', 'Kumquatfreddo', 'Kumquatfria', 'kuMQuatRUNNINg', 'LagartoBanana', 'LagostaCeleste', 'LaranjinhaFria', 'Lattugafredda', 'Lechugafria', 'LemmeDown', 'LesSean', 'LichiaNeve', 'Lighthearted1', 'LILpyro', 'Litschischnee', 'LIZardPLANTain', 'LontraAspargo', 'LooseyGoosey', 'LOquatFAlcon', 'Loroagua', 'LtManfred', 'LuckyTom', 'Luftavocado', 'lyCHEEsNOW', 'Maguedejardin', 'Maisfelice', 'Maisjoyeux', 'MANgojELLYfish', 'Mangoqualle', 'Mast3r', 'Matschsellerie', 'Mauszwiebel', 'maximus714', 'Mechantglacial', 'Medusamango', 'Melongato', 'MilhoFeliz', 'Mispelfalke', 'mOmOmO', 'MONstaSTOMPa', 'MRfoshizzle', 'MtnMan55', 'Muccaguava', 'MUDcelERY', 'muse77', 'MUSHroomBEAR', 'MutenderDreck', 'Myopic', 'Neigeaulitchi', 'Nellie4', 'Nevelitchi', 'NonSence', 'Nutmeg', 'NYX', 'Oaktr33', 'OblongShape', 'Okra-Insekt', 'okRABowl', 'OKrainSECt', 'Omangeaea', 'OOoOopont', 'OrganizeDemise', 'Osohongo', 'OstraAlho-poro', 'OtrosExpos', 'OTteraSParagUS', 'Otterspargel', 'Outb4ckm8', 'OYSTerLEEKS', 'Palomacaqui', 'PapagaioAgua', 'PAPAyaOYster', 'Parker41', 'ParlezVous', 'PatchyTheDog', 'pEARcRAB', 'peaRMud', 'Perceberabano', 'Pfefferwal', 'Pferdekurbis', 'Pigeonaukaki', 'Pilzbar', 'Pioggiakiwi', 'PlanchPlease', 'PlasticFoods', 'Platanolagarto', 'pLUMFox', 'pLUMGIRaffe', 'Plumz', 'Poirevaseuse', 'Polarbear33', 'PomboCaqui', 'Porriostrica', 'PorteSport', 'PotatoSunny', 'PotatoYo', 'ProudPraxis', 'Puerrosostra', 'PUMMELoVULTure', 'queasyqueen', 'quint007', 'RAInKIWI', 'Rando22', 'RatoCebola', 'RattyHat', 'recklessjim', 'Regenkiwi', 'ReneeNEE', 'RictusRieber', 'riiicky', 'rOgerdOger', 'runNINGshRIMP', 's1s1sly247', 'SADAlligator', 'SadbeetS', 'sadVILLAIN', 'SaiGMR', 'Saladefroide', 'SalamiJoe', 'Saleciliega', 'SammySlammy', 'SAVAGE74', 'SchnellSnail', 'SciFiNaut', 'seabreamdream', 'Sedanodifango', 'SedonaSarah', 'seebreamdream', 'shadowguo1010', 'SnKrHeAd4LiFe', 'SoccerMom', 'spartan1', 'spinACHshARK', 'SPOONPIGeon', 'SportySarah', 'SqualosSpinaci', 'StarFishKid', 'Stockman56', 'str8flya', 'StressedXx', 'SujeiraRaivosa', 'sUnNyWhAlE', 'SuperSuplex', 'swimswag', 'Tassomelone', 'TeaNCupcakes', 'Tejonmielero', 'TexugoMelao', 'Thanatos', 'Tomatenkrabbe', 'TOMatoCRAB', 'Topocipolla', 'TormentedSoulz', 'TraurigeRube', 'trendy808tracy', 'Triggah', 'Triggermi', 'Triiim', 'Tristemechant', 'TrystansTryst', 'TwinkleToejam', 'UrMyNephew', 'urPAPI', 'UrsoCogumelo', 'VacaGoiaba', 'Vacaguayaba', 'Vacheaumanioc', 'Volpeprugna', 'Vvrajuju', 'w0rw0rw0r', 'WaffleMe', 'Wasserpapagei', 'WATerparROT', 'WEAKpumpKIN', 'wiseguywilly', 'WutenderDreck', 'ZaHaD', 'ZebraTrovao', 'Zebratuono', 'zeus_lightning', 'Zitronenkojote', 'Zorrociruela', 'ZUCChinishriIMP', 'zZpilgrimZz', 'THUNderzeBRA', 'OOpsCuraziest']):
+#Carregamento das bibliotecas
+from google.cloud import vision
+import os
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "CodMCheckBot-712670c4ff95.json"
+client = vision.ImageAnnotatorClient()
+
+#dados
+listaNomes = ['nyx', 'zahad', 'kidd0', 'plumz', 'recku', 'triim', 'gemma', 'inews', 'runner', 'nutmeg', 'myopic', 'avgjoe', 'momomo', 'hooray', 'deltat', 'saigmr', 'wrecku', 'triiim', 'urpapi', 'mast3r', 'muse77', 'mom0mo', 'kool21', 'f82fei', 'hazmatt', 'axaaxel', 'triggah', 'foxsnow', 'pilzbar', 'rando22', 'guywick', 'lessean', 'bolokra', 'campftw', 'pearmud', 'illyeah', 'nellie4', 'cyndnay', 'cougar7', 'happybo', 'blazing', 'oaktr33', 'lilpyro', 'plumfox', 'riiicky', 'kiwiant', 'fiverify', 'waffleme', 'bruddah5', 'killerli', 'savage74', 'hypnotyz', 'figsfast', 'bowibear', 'digitpti', 'chougras', 'parker41', 'rainkiwi', 'luckytom', 'briguy40', '1trippin', 'gibbytfx', 'crabhero', 'okrabowl', 'carsonia', 'orworwor', 'guavacow', 'quint007', 'spartan1', 'athena49', 'sadbeets', 'swimswag', 'pearcrab', 'lodopera', 'potatoyo', 'dipszips', 'ibenboyl', 'jumper62', 'osohongo', 'reneenee', 'clownsey', 'finagler', 'loroagua', 'nonsence', 'rattyhat', 'firehala', '64andtim', 'itrippin', 'sueñocol', 'mtnman55', 'vvrajuju', 'thanatos', 'edwinftw', 'bombboys', 'str8flya', 'cart808s', 'solomon22', 'mudcelery', 'ayeklutch', 'heroleeks', 'vilãofrio', 'happyhero', 'lemmedown', 'dogdaikon', 'ooooopont', '1aparquet', 'fungoorso', 'aaaannika', 'soccermom', 'akanaclan', '180teresa', 'outb4ckm8', 'happyrain', 'domin4ter', 'ltmanfred', 'boxboiboy', 'salamijoe', 'catalyze1', 'burroyaca', 'w0rw0rw0r', 'dogs4lyf3', 'elbowdrop', 'triggermi', 'trystanst', 'megatony2', 'jennifer7', 'kneeldown', 'melóngato', 'baumgecko', 'dirtangry', 'jimbolaya', 'fishersea', 'iaparquet', 'omangeaea', 'abcoukits', 'guavenkuh', 'scifinaut', "bold'ours", 'catalyzel', 'duriánbol', 'spinathai', 'regenkiwi', 'melongato', 'happycorn', 'mouseonlon', 'khakitaube', 'scifi̇naut', 'grassmango', 'hormigauva', 'boldegombo', 'lichianeve', 'cipherzero', 'carsmitter', 'parlezvous', 'heróifeliz', 'ursotigela', '81sunset06', 'maisfelice', 'nievezorro', 'bobbodding', 'jameston46', 'canedaikon', 'gymrat1987', 'gunschidri', 'firewalker', 'ratocebola', 'skylobster', 'portesport', 'daikonfrog', 'kenjiaoa0a', 'muccaguava', 'apiodelodo', 'airavocado', 'pfefferwal', 'asinogiaca', 'rogerdoger', 'milhofeliz', 'maikafresh', 'baxterrama', 'aniseantes', 'demondanny', 'okrainsect', 'ciambamfam', 'tatertoter', 'outb4ckm00', 'sunnywhale', 'fatcabbage', 'cherrysalt', 's1s1sly247', 'colhormiga', 'maximus714', 'dalkonfrog', 'anklebiter', 'facemelter', 'cptobvious', 'zangãocopo', 'stressedxx', 'iycheesnow', '2cute2quit', 'nevelitchi', 'sadvillain', 'bowldurian', 'colerunner', 'lycheesnow', 'clambamfam', 'urmynephew', 'kiwilluvia', 'ranadaikon', 'daikonfro9', 'kirschsalz', 'aneaujaque', 'sunnywhaie', 'antcabbage', 'zebratuono', '4poth4c4ry', 'vacagoiaba', 'tomatocrab', 'héroefeliz', 'otrosexpos', 'maisjoyeux', 'alfacefria', 'daggerone8', 'crustedpie', 'stockman56', 'pombocaqui', 'ostramamão', 'sedonasarah', 'chromebling', 'boldemanioc', 'joysterlees', 'zebratrovão', 'queasyqueen', 'sammyslammy', 'recklessjim', 'iseguywilly', 'coiotelimão', 'zzpilgrimzz', 'dreamdragon', 'luftavocado', 'oysterleeks', 'figoligeiro', 'vacaguayaba', 'medusamango', 'daikonperro', 'daikónperro', 'mrfoshizzle', 'zebratrovao', 'jamminjerry', 'texugomelao', 'kumquatfria', 'pioggiakiwi', 'starfishkid', 'potatosunny', 'citrussting', 'coldkumquat', 'proudpraxis', 'bowicassava', 'formicavolo', 'lechugafria', 'airkangaroo', 'commentsten', 'ggbooyakagg', 'horsesquash', 'dragãosonho', 'coyotelimón', 'volpeprugna', 'palomacaqui', 'weakpumpkin', 'sportysarah', 'kalteorange', 'cerisesalee', 'fichiveloci', 'mispelfalke', 'doninhacoco', 'hummerblitz', 'heurexheros', 'donnerzebra', 'dontshootme', '180horrific', 'chaosexists', 'gingersnake', 'coldlettuce', 'villanofrio', 'oblongshape', 'avocadoaria', 'burroacelga', 'bokchoyasno', 'kiwidepluie', 'kumquatfría', 'saldecerezo', 'plumgiraffe', 'insettoocra', 'coyotelimon', 'pombocolher', 'pepperwhale', 'ggb0oyakagg', 'ostrapapaya', 'waterparrot', 'texugomelão', 'hundedaikon', 'spoonpigeon', 'billyballer', 'tassomelone', 'vilãotriste', 'okra-insekt', 'mauszwiebel', 'figuevéloce', 'polarbear33', 'topocipolla', 'supersuplex', 'coldvillain', 'saleciliega', 'insectookra', 'coiotelimao', 'kiwichuvoso', 'mangoqualle', 'airkangargo', 'cebratrueno', 'otterspargel', 'craziisnooty', 'papagaioagua', 'aneaubokchoy', 'puerroshéroe', 'cebollaraton', 'dragonreveur', 'porriostrica', 'saleciliegia', 'muccamanioca', 'felonyfeline', 'zorrociruela', 'insetoquiabo', 'wiseguywilly', 'sadalligator', 'vacamandioca', 'cassetetaper', 'mushroombear', 'jacarétriste', 'homardéclair', 'rictusrieber', 'pferdekürbis', 'sada|ligator', 'delfindurián', 'teancupcakes', 'kaijustomper', 'octopusonion', 'cavallozucca', 'saladefroide', 'homardeclair', 'dreamerdonna', 'abutrepomelo', 'asinobokchoy', 'monstastompa', 'poirevaseuse', 'plasticfoods', 'austernlauch', 'furettococco', 'colgrasienta', 'planchplease', 'raicesdeyuca', 'tigelaquiab0', 'delfinozucca', 'delfinkurbis', 'delfindurian', 'irafaciruela', 'schnellsnail', 'caimántriste', 'friedchicken', 'pongfonglong', 'falconespola', 'puerrosostra', 'bunsenburner', 'thunderzebra', 'averagelemon', 'loquatfalcon', 'himbeerkafer', 'cabbagedream', 'mirtilomagro', 'pferdekurbis', 'cupbumblebee', 'crookedteeth', 'averagesloth', 'traurigerube', 'patchythedog', 'cassavaroots', 'celerivaseux', 'tejonmielero', 'papayaoyster', '808crapscrus', 'doloreskaffe', 'ursocogumelo', 'tigeladurião', 'avocataerien', 'abacateaereo', 'looseygoosey', 'pigeonaukaki', 'girafaameixa', 'd4rksorc3ror', 'cebollapulp0', 'spinachshark', 'coyotelimone', 'geckoarboreo', 'granchiopera', 'hamcroissant', 'kumquafreddo', 'heureuxhéros', 'jirafaciruela', 'héroecangrejo', 'grapefruitant', 'birdartichoke', 'snkrhead4life', 'drumersporter', 'cavaloabóbora', 'homardcéleste', 'owoquiessence', 'bustedkneecap', 'unfashionlsta', 'baleiapimenta', 'murdermurdock', 'prophecymikey', 'aoa0aeatx0x0x', 'esel-chicorée', 'dolphinsquash', 'himmeishummer', 'celeryjumping', 'neigeaulitchi', 'geckofruitier', 'seebreamdream', 'o0pscuraziest', 'tristemechant', 'squalospinaci', 'aipolamacento', 'cantxxxsavage', 'fastfanatic22', 'cassettetaper', 'schnellefeige', 'esel-chicoree', 'jacareabobora', 'lontraaspargo', 'aoaoaeatx0x0x', 'homardceleste', 'perceberabano', 'chienaudaikon', 'daikon-frosch', 'oopscuraziest', 'furiadetierra', 'formicacavolo', 'elitenobleman', 'shadowguo1010', 'cangurodeaire', 'chayoteparrot', 'cobragengibre', 'maguedejardin', 'halconnispero', 'comadrejacoco', 'fourmisaukiwi', 'vacheaumanioc', 'bobobobangels', 'lighthearted1', 'cherriothecat', 'villanotriste', 'wasserpapagei', 'calabazadébil', 'insectaverage', 'coconutweasel', 'kingslayer777', 'halcónnispero', 'runningshrimp', 'backscratcher', 'piccionecachi', 'gafanhotonabo', 'kumquatfreddo', 'celerisauteur', 'limãomediocre', 'catcantaloupe', 'perceberábano', 'filmcritic884', 'lagartobanana', 'crabeàlapoire', 'cinnamonsugar', 'bouecolérique', 'sedanodifango', 'peralamacenta', 'lattugafredda', 'crabeheroique', 'barataraivosa', 'mutenderdreck', 'seabreamdream', 'litschischnee', 'tomatenkrabbe', 'crabealapoire', 'cavaloabobora', 'twinkletoejam', 'trystanstryst', 'palomacuchara', 'wutenderdreck', 'lagostaceleste', 'laranjinhafria', 'cangrejotomate', 'cantsxxxsavage', '45snootym4st3r', 'ingwerschlange', 'mechantglacial', 'vacheàlagoyave', 'zitronenkojote', 'otterasparagus', 'manguedejardin', 'eastnewyorkgee', 'jingerschlange', 'caranguejopera', 'trashcankicker', 'chayotepapagei', 'ostraalho-poro', 'insecteaugombo', 'tamarindocabra', 'pummelovulture', 'organizedemise', 'zeus_lightning', 'crabealatomate', 'água-vivamanga', 'tormentedsoulz', 'balenapiperita', 'squalosspinaci', 'platanolagarto', 'radishbarnacle', 'matschsellerie', 'honigtau-dachs', 'kumquatrunning', 'cavallettarapa', 'trendy808tracy', 'golfinhodurião', 'chicharosfresa', 'formigarepolho', 'mangojellyfish', 'sujeiraraivosa', 'honeydewbadger', 'aguacatedeaire', 'ballenasoleada', 'zucchinishrimp', 'gattocantalupo', '808clutchpoker', 'honigtau-dechs', 'lizardplantain', 'zucchinishriimp']
+class Namelist(object):
+    def sort(self):
+    
+        self.names = sorted(self.names, key=len)
+        #print(self.names)
+        self.lenths = [len(self.names[0])]
+        self.lenthStart = [0]
+
+        for index,i in enumerate(self.names):
+            if len(i) > self.lenths[len(self.lenths) - 1]:
+                self.lenths.append(len(i))
+                self.lenthStart.append(index)
+    
+    def __init__ (self,namesList,lenths=[],lenthStart=[]):
+        
+        '''namesList = the names sorted by lenth
+        lenth[index] = the respective lenth
+        lenthStart[index] = where lenth[start] starts on namesList
+        '''
+        
+        
+        if type(lenths) != list or type(lenthStart) != list :
+            try:
+                raise AttributeError ('All arguments must be lists')
+            except AttributeError:
+                raise
+        
+        if len(lenths) != len(lenthStart):
+            try:
+                raise IndexError ('Both lenth lists should have the same lenth')
+            except IndexError:
+                raise
+        self.names = namesList
+        self.lenths = lenths
+        self.lenthStart = lenthStart
+        if lenths == [] and lenthStart == [] :
+            self.sort()
+        
+        
+        
+    def check(self,name):
+        '''returns True if name is in self.names
+        False if name isnt in self.names'''
+        tamanho = len(name)
+        try:
+            index = self.lenths.index(tamanho)
+        except ValueError:
+            return False #nesse caso a lista nao possui elementos com o tamanho desejado
+
+
+        try:
+            self.lenthStart[index+1]
+
+        except IndexError:
+            #the name has the biggest lenth avaliable
+
+            if name in self.names[self.lenthStart[index] : ]  :
+                return True
+
+
+        else:
+            if name in self.names[self.lenthStart[index] : self.lenthStart[index+1] ]:
+                return True
+
+        return False
+
+
+listaNomes = Namelist(listaNomes)    
+        
+        
+
+
+    
+          
+
+
+def bot_counter(path, width, lista_nomes = listaNomes):
   with open(path, 'rb') as image_file:
     content = image_file.read()
-
-  #Carregamento das bibliotecas
-  from google.cloud import vision
-  import os
-  os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "CodMCheckBot-712670c4ff95.json"
+    
 
   #Leitura OCR
-  client = vision.ImageAnnotatorClient()
   image = vision.types.Image(content=content)
   response = client.text_detection(image=image)
   texts = response.text_annotations
-
+ 
   retorno = [] #Lista de retorno
-  palavras = texts[0].description.split() #Variável com o texto capturado
+
+  try:
+      palavras = texts[0].description.split() #Variável com o texto capturado
+  except IndexError:
+    return [],[]
+
   #Checa se existem 120 ou menos palavras na leitura
   if len(palavras) > 120:
     try:
         raise RuntimeError ('Image input had too many words, thus out of range for this function')
     except RuntimeError:
         raise
+  
+    
 
-  for nome in lista_nomes:
-    for index in range(len(palavras)):
-        if nome.lower() in palavras[index].lower():
+  for index,nome in enumerate(palavras):
+        if nome[1:4] == 'BOT':
+            print(nome)
+            for i in range ( len(nome) - 2) :
+                if lista_nomes.check( nome[ i : ].lower() ):
+                    retorno.append([nome[ i : ], texts[index+1].bounding_poly])
+                    continue
+        if lista_nomes.check( nome.lower() ) :
             retorno.append([nome, texts[index+1].bounding_poly])
-            break
-
+        
+        
   time_a = []
   time_b = []
   for i in retorno:
@@ -39,4 +130,4 @@ def bot_counter(path, width, lista_nomes = ['180Horrific', '1AParquet', '1Trippi
       
   return(time_a, time_b)
 
-  
+
